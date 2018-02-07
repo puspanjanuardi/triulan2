c13c5c11b6b925de193fae1aba3c0935c1541a59
puspanjanuardi <puspanjanuardi@gmail.com>
2018-02-07 16:07:04 +0700

    ujian

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Responsive</title><link rel="stylesheet" type="text/css" href="css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
+		<link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+	</head>
+	<body>
+		<div id="container" class="clearfix">
+			<div id="header" class="clearfix">
+				<ul class="clearfix">
+					<div class="list clearfix">
+						<li><a href="#"><i class="fa fa-home" aria-hidden="true"></i></a></li>
+						<li><a href="#"><i class="fa fa-bars" aria-hidden="true"></i><span>Categories</span></a></li>
+						<li><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Shop</span></a></li>
+						<li><a href="#"><i class="fa fa-search" aria-hidden="true"></i><span>Search</span></a></li>
+					</div>
+					<div class="list clearfix">
+						<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i><span>Follow Us</span></a></li>
+						<li><a href="#"><i class="fa fa-user" aria-hidden="true"></i><span>Sign in</span></a></li>
+					</div>
+				</ul>
+			</div>
+			<h1>RESPONSIVE</h1>
+			<div id="wrapper" class="clearfix">
+				<div id="content" class="clearfix">
+					<h2>The Future Of The Web : Firefox Quantum</h2>
+					<p class="sub">Doru Ciobanu &bullet; <a href="#">News</a> &bullet; November 16, 2017</p>
+					<div id="img" class="clearfix"><img src="images/LogoTagline.png"></div>
+					<p> Fusce ac mauris feugiat, rutrum augue id, tincidunt libero. Vivamus tincidunt nulla vitae 
+					turpis sodales, id posuere risus iaculis. Duis sed nulla at felis dignissim bibendum at non est. 
+					Phasellus et velit eget tortor lacinia iaculis. Cras sed orci ut est fermentum tempor et id turpis.
+					</p>
+					<p>
+					Morbi vel maximus metus. Proin accumsan a risus id accumsan. Pellentesque auctor et lectus nec facilisis.
+					Fusce lobortis non nisi eget blandit. Maecenas lacus urna, tempor at libero in, facilisis tempor diam. 
+					In sed nulla diam. Pellentesque ac lacus facilisis, commodo eros ut, efficitur erat.
+					</p>
+					<div id="more"><a href="#">Read More</a></div>
+				</div>
+				<div id="sidebar" class="clearfix">
+					<h3>Our Product</h3>
+					<a href="#">Show More</a>
+					<ul>
+						<li class="clearfix">
+							<div class="img clearfix"><img src="images/money.jpg"></div>
+							<ul>
+								<li><h4>FRAMEWORK 1</h4></li>
+								<li><p>Lorem Ipsum V2</p></li>
+								<li><p>$100</p></li>
+							</ul>
+						</li>
+						<li class="clearfix">
+							<div class="img clearfix"><img src="images/money.jpg"></div>
+							<ul>
+								<li><h4>FRAMEWORK 1</h4></li>
+								<li><p>Lorem Ipsum V2</p></li>
+								<li><p>$100</p></li>
+							</ul>
+						</li>
+						<li class="clearfix">
+							<div class="img clearfix"><img src="images/sedekah1.jpeg"></div>
+							<ul>
+								<li><h4>FRAMEWORK 1</h4></li>
+								<li><p>Lorem Ipsum V2</p></li>
+								<li><p>$100</p></li>
+							</ul>
+						</li>
+						<li class="clearfix">
+							<div class="img clearfix"><img src="images/sedekah1.jpeg"></div>
+							<ul>
+								<li><h4>FRAMEWORK 1</h4></li>
+								<li><p>Lorem Ipsum V2</p></li>
+								<li><p>$100</p></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
